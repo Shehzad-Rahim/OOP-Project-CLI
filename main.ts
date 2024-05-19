5987aabe39dd3a00655f0874e0c59081da7a1937
Shehzad-Rahim <shehzadrahim33@gmail.com>
2024-05-19 14:27:01 +0500

    project no.10

---
+++ main.ts
@@ -0,0 +1,59 @@
+import inquirer from "inquirer";
+
+class Student {
+  name:string
+  constructor(n:string){
+      this.name = n;
+  }
+}
+
+class Person {
+  students:Student[]=[]
+
+  addStudent(obj:Student){
+    this.students.push(obj)
+  }
+}
+
+const persons = new Person()
+
+const programStart = async(persons:Person) => {
+  do{
+    const ans = await inquirer.prompt({
+      name:'select',
+      type:'list',
+      message:'Who do you want to talk to?',
+      choices:['Self','student']
+    })
+  
+    if(ans.select == "Self"){
+      console.log('Hello I am talking to myself');
+      console.log('I am doing well');
+      
+    }
+    if(ans.select == "student"){
+    const ans = await inquirer.prompt({
+      name:'student',
+      type: 'input',
+      message: 'Which student do you want to connect'
+    });
+    const student = persons.students.find(val => val.name == ans.student)
+  
+    if(!student){
+      const name = new Student(ans.student)
+      persons.addStudent(name)
+  
+      console.log(`Hello i am ${name.name}, and I am good `);
+      console.log(persons.students);
+      
+    }
+  
+    if(student){
+      console.log(`Hello i am ${student.name}, and I am good..... `);
+      console.log(persons.students);
+    }
+    }
+  }while(true)
+}
+
+programStart(persons)
